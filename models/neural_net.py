59522ce0ad9241b19c2fdea7ebbc1fc94b305038
Emily Su <emilyzqsu@gmail.com>
2025-08-04 02:44:25 -0400

    Finished neural network script and updated requirements.txt

--- models/neural_net.py
+++ models/neural_net.py
@@ -7,21 +7,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+import joblib
 
 """
-Script implementing a neural network model that uses the Adam optimizer.
+Script implementing a feed-forward neural network model that uses the Adam optimizer.
 """
 # Global variables 
 batch_size = 10 # Batch size
 learning_rate = 0.0001 # Learning rate
 epochs_num = 100 # Epochs
-hidden_size = 50
+hidden_size = 50 # Default hidden layer dimension
 output_size = 1
-reg_penalty = 0.01 # Regularization penalty (Prevents overfitting)
+reg_penalty = 0.001 # Regularization penalty (Prevents overfitting)
 max_features_num = 80000 # To reduce the number of dimensions!
 
-torch.autograd.set_detect_anomaly(True)
+torch.autograd.set_detect_anomaly(True) # Stops the script if it encounters any errors
 
 class NeuralNetwork(nn.Module):
     """A neural network that inherits from PyTorch's neural network"""
@@ -45,8 +49,7 @@ def vectorize_features(the_dataset):
     # https://scikit-learn.org/stable/modules/generated/sklearn.compose.ColumnTransformer.html
     # Vectorize 
     ct_features = ColumnTransformer(
-        transformers = [("division", TfidfVectorizer(max_features = max_features_num), "division"),
-         ("dept", TfidfVectorizer(max_features = max_features_num), "dept"),
+        transformers = [
          ("course", TfidfVectorizer(max_features = max_features_num), "course"),
          ("term", TfidfVectorizer(max_features = max_features_num), "term"),
          ("year", MinMaxScaler(), ["year"]),
@@ -58,7 +61,6 @@ def vectorize_features(the_dataset):
          ("item 6", MinMaxScaler(), ["item 6 (overall, the quality of my learning experience in the course was:)"]),
          ("instructor generated enthusiasm", MinMaxScaler(), ["instructor generated enthusiasm"]),
          ("course workload", MinMaxScaler(), ["course workload"]),
-         ("i would recommend this course", MinMaxScaler(), ["i would recommend this course"]),
          ("last name", TfidfVectorizer(max_features = max_features_num), "last name"),
          ("description", TfidfVectorizer(stop_words = 'english', max_features = max_features_num), "description"
           )],
@@ -120,9 +122,10 @@ def convert_to_dataloaders(train_x, train_y, validation_x, validation_y, test_x,
     
     return training_set_dataloader, validation_set_dataloader, test_set_dataloader
 
-def training_nn(nn_model, training_set):
+def training_nn(nn_model, training_set, type_of_set, is_validating):
     """This function trains the neural network <nn_model> using the binary-cross entropy as the loss function
-    and Adam to optimize its parameters (weights and biases)"""
+    and Adam to optimize its parameters (weights and biases). It uses <training_set> as the data and the type of set 
+    is represented by <type_of_set>. <is_validating> indicates if training_nn is being called by the validation function or not."""
     loss_function = nn.BCELoss()
     objective_function = torch.optim.Adam(nn_model.parameters(), lr = learning_rate, weight_decay = reg_penalty)
     
@@ -148,24 +151,63 @@ def training_nn(nn_model, training_set):
         print(f"Epoch: {epoch}, Average Loss: {average_loss_per_epoch}")
         training_loss_vals += [average_loss_per_epoch]
     
-    # Create visualization 
-    plt.title("Average loss over epochs")
-    plt.xlabel("Epochs")
-    plt.ylabel("Average Loss")
-    plt.plot(range(len(training_loss_vals)), np.array(training_loss_vals))
-    plt.savefig("graphs/epochs_neural_network.png")
-    plt.show()
+    if is_validating is False: # This means that we are not finding the optimal hidden dimension number with this function
+        # Create visualization 
+        plt.figure()
+        plt.title(f"Average loss over epochs for {type_of_set} set")
+        plt.xlabel("Epochs")
+        plt.ylabel("Average Loss")
+        plt.plot(range(len(training_loss_vals)), np.array(training_loss_vals))
+        plt.savefig(f"graphs/epochs_neural_network_{type_of_set}.png")
     
-    return nn_model
-        
-def validation():
-    """This function iterates through different values for the batch size and the learning rate"""
+    return min(training_loss_vals)
 
-def prediction():
-    """"""
-def shap():
-    """"""
+def validation(validation_set, k, input_size): 
+    """Return the lowest loss for the neural network for the hidden layer dimension, <k>"""
+    nn_model = NeuralNetwork(input_size, k, output_size)
+    return training_nn(nn_model, validation_set, "validation", True)
 
+def prediction(testing_set, nn_model):
+    """Make predictions and return the accuracy value, predicted labels, and actual labels of <testing_set>."""
+    # Referenced https://www.datacamp.com/tutorial/pytorch-tutorial-building-a-simple-neural-network-from-scratch
+    # for loop
+    predicted_labels = []
+    actual_labels = []
+    total = 0
+    correct = 0
+    with torch.no_grad():
+        for datapoint, actual_label in testing_set:
+            # Get prediction from model 
+            prediction = nn_model(datapoint)  
+            # Apply threshold to get predicted labels (1 = is recommended, 0 = somewhat recommended/not recommended)
+            predicted_label = np.where(prediction >= 0.5, 1, 0) 
+            # Flatten predictions and actual labels
+            predicted_label = np.array(list(itertools.chain(*predicted_label))) 
+            actual_label = np.array(actual_label).flatten()
+            # Append 
+            predicted_labels.append(predicted_label)
+            actual_labels.append(actual_label)
+            # Add to the correct and total variables
+            correct += (predicted_label == actual_label).sum()  # Count correct predictions
+            total += actual_label.shape[0]
+    # Calculate accuracy 
+    accuracy_val = correct/total
+    return accuracy_val, predicted_labels, actual_labels
+
+def create_report_confusion_matrix(predicted_labels, actual_labels):
+    """Prints out classification report and a confusion matrix visualization."""
+    # Referenced: https://www.datacamp.com/tutorial/pytorch-tutorial-building-a-simple-neural-network-from-scratch
+    # Print out classification report
+    predicted_labels = list(itertools.chain(*predicted_labels))
+    actual_labels = list(itertools.chain(*actual_labels))
+    print(classification_report(actual_labels, predicted_labels))
+    
+    # Create confusion matrix visualizatiuon
+    confusion_matrix_results = confusion_matrix(actual_labels, predicted_labels)
+    plt.figure()
+    sns.heatmap(confusion_matrix_results, annot=True, cbar=False, fmt="g")
+    plt.savefig(f"graphs/confusion_matrix_neural_network.png")
+    
 if __name__ == '__main__':
     # Referenced: https://docs.pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
     # Referenced: https://machinelearningmastery.com/develop-your-first-neural-network-with-pytorch-step-by-step/
@@ -190,11 +232,53 @@ if __name__ == '__main__':
     # print(test_y.shape)
     # Convert the datasets into dataloaders for the neural network
     training_set_dataloader, validation_set_dataloader, test_set_dataloader = convert_to_dataloaders(train_x, train_y, validation_x, validation_y, test_x, test_y)
-    
-    # Create neural network model (autoencoder)
     input_size = train_x.shape[1]
-    nn_model = NeuralNetwork(input_size, hidden_size, output_size)
-    # print(nn_model)
     
-    # Train neural network
-    trained_nn_model = training_nn(nn_model, training_set_dataloader)
+    # Test different values for hidden_size 
+    k_values = [16, 30, 50, 60, 65]
+    all_loss = []
+    lowest_loss = 1.0
+    lowest_loss_k = hidden_size 
+    for k in k_values: 
+        a_loss = validation(validation_set_dataloader, k, input_size)
+        all_loss.append(a_loss)
+        if lowest_loss > a_loss: 
+            lowest_loss = a_loss
+    hidden_size = lowest_loss_k
+    print(f"Hidden dimension that gives the lowest loss: {hidden_size}")
+
+    # Create graph for validation for hidden_size
+    plt.figure()
+    plt.title(f"Average loss over different number of nodes in hidden layer")
+    plt.xlabel("Number of nodes in hidden layer")
+    plt.ylabel("Average Loss")
+    plt.plot(k_values, np.array(all_loss))
+    plt.savefig(f"graphs/hidden_nodes_neural_network.png")
+
+    # Create and train neural networks on chosen value for hidden_size
+    # On validation set (Comment out if you want to run)
+    nn_model_validation = NeuralNetwork(input_size, hidden_size, output_size)
+    training_nn(nn_model_validation, validation_set_dataloader, "validation", False)
+    # On training set
+    nn_model = NeuralNetwork(input_size, hidden_size, output_size)
+    training_nn(nn_model, training_set_dataloader, "training", False)
+    
+    # Save the trained model 
+    joblib.dump(nn_model, "models/saved_models/neural_network.sav")
+    
+    # Load back in trained model 
+    loaded_nn_model = joblib.load("models/saved_models/neural_network.sav")
+    # Make predictions 
+    # On training set 
+    accuracy_val_training, predicted_labels_training, actual_labels_training = prediction(training_set_dataloader, loaded_nn_model)
+    # On validation set 
+    accuracy_val_validation, predicted_labels_validation, actual_labels_validation = prediction(validation_set_dataloader, loaded_nn_model)
+    # On test set
+    accuracy_val_test, predicted_labels_test, actual_labels_test = prediction(test_set_dataloader, loaded_nn_model)
+    # Print out the accuracies based on the different sets 
+    print(f'Accuracy on training set: {accuracy_val_training}')
+    print(f'Accuracy on validation set: {accuracy_val_validation}')
+    print(f'Accuracy on test set: {accuracy_val_test}')
+    
+    # Confusion matrix
+    create_report_confusion_matrix(predicted_labels_test, actual_labels_test)
